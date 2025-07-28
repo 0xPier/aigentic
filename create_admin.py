c97eada2b7ea5185783d4a2e748a1d1443485914
0xPier <sydbarzanti@gmail.com>
2025-07-28 14:44:40 +0200

    Update dockerignore to include create_admin.py script

--- create_admin.py
+++ create_admin.py
@@ -9,7 +9,7 @@ import argparse
 from pathlib import Path
 
 # Add project root to the Python path
-project_root = Path(__file__).parent.absolute()
+project_root = Path(__file__).parent.parent.absolute()
 sys.path.insert(0, str(project_root))
 
 from src.core.config import settings
